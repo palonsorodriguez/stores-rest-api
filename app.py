b1ec44b47dcb354c317582c1486fde3965427368
palonsorodriguez <60453543+palonsorodriguez@users.noreply.github.com>
2021-10-13 00:01:28 +0100

    updating code to replace postgres:// for postgresql:// in the connection string

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +9,15 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # 2 PARAMETERS, 1 the one to read 2 the default
+
+#https://help.heroku.com/ZKNTJQSK/why-is-sqlalchemy-1-4-x-not-connecting-to-heroku-postgres
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # 2 PARAMETERS, 1 the one to read 2 the default
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db') # 2 PARAMETERS, 1 the one to read 2 the default
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Turns off flask sql alchemy modification tracker, doesn't turn off sql alchemy modificaion tracker
 # #Only changing the extensions behaviour not the underlying sqlalchemy behaviour
 app.secret_key = 'jose'
